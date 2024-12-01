f96dcd9b5d826f0cb69dae587a84ee23dbff325c
Usama Tariq <tariqosama1999@gmail.com>
2024-12-02 00:47:49 +0500

    Added Files

---
+++ MyApp/Form1.Designer.cs
@@ -0,0 +1,374 @@
+﻿
+namespace MyApp
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.n1 = new System.Windows.Forms.Button();
+            this.n2 = new System.Windows.Forms.Button();
+            this.n4 = new System.Windows.Forms.Button();
+            this.n5 = new System.Windows.Forms.Button();
+            this.n6 = new System.Windows.Forms.Button();
+            this.n7 = new System.Windows.Forms.Button();
+            this.n8 = new System.Windows.Forms.Button();
+            this.n9 = new System.Windows.Forms.Button();
+            this.nEquals = new System.Windows.Forms.Button();
+            this.nAdd = new System.Windows.Forms.Button();
+            this.nSubtract = new System.Windows.Forms.Button();
+            this.nMultiply = new System.Windows.Forms.Button();
+            this.nPoint = new System.Windows.Forms.Button();
+            this.n0 = new System.Windows.Forms.Button();
+            this.nCE = new System.Windows.Forms.Button();
+            this.nDivide = new System.Windows.Forms.Button();
+            this.nClear = new System.Windows.Forms.Button();
+            this.n3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Snow;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.textBox1.Font = new System.Drawing.Font("Palatino Linotype", 29.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Black;
+            this.textBox1.Location = new System.Drawing.Point(13, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(280, 60);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // n1
+            // 
+            this.n1.BackColor = System.Drawing.Color.Crimson;
+            this.n1.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.n1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.n1.Font = new System.Drawing.Font("Goudy Old Style", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n1.Location = new System.Drawing.Point(12, 102);
+            this.n1.Name = "n1";
+            this.n1.Size = new System.Drawing.Size(64, 62);
+            this.n1.TabIndex = 1;
+            this.n1.Text = "1";
+            this.n1.UseVisualStyleBackColor = false;
+            this.n1.Click += new System.EventHandler(this.n1_Click);
+            // 
+            // n2
+            // 
+            this.n2.BackColor = System.Drawing.Color.Crimson;
+            this.n2.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.n2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.n2.Font = new System.Drawing.Font("Goudy Old Style", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n2.Location = new System.Drawing.Point(82, 102);
+            this.n2.Name = "n2";
+            this.n2.Size = new System.Drawing.Size(64, 62);
+            this.n2.TabIndex = 2;
+            this.n2.Text = "2";
+            this.n2.UseVisualStyleBackColor = false;
+            this.n2.Click += new System.EventHandler(this.n2_Click);
+            // 
+            // n4
+            // 
+            this.n4.BackColor = System.Drawing.Color.Crimson;
+            this.n4.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.n4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.n4.Font = new System.Drawing.Font("Goudy Old Style", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n4.Location = new System.Drawing.Point(12, 170);
+            this.n4.Name = "n4";
+            this.n4.Size = new System.Drawing.Size(64, 62);
+            this.n4.TabIndex = 4;
+            this.n4.Text = "4";
+            this.n4.UseVisualStyleBackColor = false;
+            this.n4.Click += new System.EventHandler(this.n4_Click);
+            // 
+            // n5
+            // 
+            this.n5.BackColor = System.Drawing.Color.Crimson;
+            this.n5.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.n5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.n5.Font = new System.Drawing.Font("Goudy Old Style", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n5.Location = new System.Drawing.Point(83, 170);
+            this.n5.Name = "n5";
+            this.n5.Size = new System.Drawing.Size(64, 62);
+            this.n5.TabIndex = 5;
+            this.n5.Text = "5";
+            this.n5.UseVisualStyleBackColor = false;
+            this.n5.Click += new System.EventHandler(this.n5_Click);
+            // 
+            // n6
+            // 
+            this.n6.BackColor = System.Drawing.Color.Crimson;
+            this.n6.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.n6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.n6.Font = new System.Drawing.Font("Goudy Old Style", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n6.Location = new System.Drawing.Point(153, 170);
+            this.n6.Name = "n6";
+            this.n6.Size = new System.Drawing.Size(64, 62);
+            this.n6.TabIndex = 6;
+            this.n6.Text = "6";
+            this.n6.UseVisualStyleBackColor = false;
+            this.n6.Click += new System.EventHandler(this.n6_Click);
+            // 
+            // n7
+            // 
+            this.n7.BackColor = System.Drawing.Color.Crimson;
+            this.n7.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.n7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.n7.Font = new System.Drawing.Font("Goudy Old Style", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n7.Location = new System.Drawing.Point(12, 238);
+            this.n7.Name = "n7";
+            this.n7.Size = new System.Drawing.Size(64, 62);
+            this.n7.TabIndex = 7;
+            this.n7.Text = "7";
+            this.n7.UseVisualStyleBackColor = false;
+            this.n7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // n8
+            // 
+            this.n8.BackColor = System.Drawing.Color.Crimson;
+            this.n8.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.n8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.n8.Font = new System.Drawing.Font("Goudy Old Style", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n8.Location = new System.Drawing.Point(83, 238);
+            this.n8.Name = "n8";
+            this.n8.Size = new System.Drawing.Size(64, 62);
+            this.n8.TabIndex = 8;
+            this.n8.Text = "8";
+            this.n8.UseVisualStyleBackColor = false;
+            this.n8.Click += new System.EventHandler(this.n8_Click);
+            // 
+            // n9
+            // 
+            this.n9.BackColor = System.Drawing.Color.Crimson;
+            this.n9.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.n9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.n9.Font = new System.Drawing.Font("Goudy Old Style", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n9.Location = new System.Drawing.Point(152, 238);
+            this.n9.Name = "n9";
+            this.n9.Size = new System.Drawing.Size(64, 62);
+            this.n9.TabIndex = 9;
+            this.n9.Text = "9";
+            this.n9.UseVisualStyleBackColor = false;
+            this.n9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // nEquals
+            // 
+            this.nEquals.BackColor = System.Drawing.Color.LightPink;
+            this.nEquals.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.nEquals.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.nEquals.Font = new System.Drawing.Font("Goudy Old Style", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nEquals.Location = new System.Drawing.Point(12, 376);
+            this.nEquals.Name = "nEquals";
+            this.nEquals.Size = new System.Drawing.Size(203, 56);
+            this.nEquals.TabIndex = 10;
+            this.nEquals.Text = "=";
+            this.nEquals.UseVisualStyleBackColor = false;
+            this.nEquals.Click += new System.EventHandler(this.nEquals_Click);
+            // 
+            // nAdd
+            // 
+            this.nAdd.BackColor = System.Drawing.Color.Crimson;
+            this.nAdd.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.nAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.nAdd.Font = new System.Drawing.Font("Goudy Old Style", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nAdd.Location = new System.Drawing.Point(229, 102);
+            this.nAdd.Name = "nAdd";
+            this.nAdd.Size = new System.Drawing.Size(64, 62);
+            this.nAdd.TabIndex = 11;
+            this.nAdd.Text = "+";
+            this.nAdd.UseVisualStyleBackColor = false;
+            this.nAdd.Click += new System.EventHandler(this.nAdd_Click);
+            // 
+            // nSubtract
+            // 
+            this.nSubtract.BackColor = System.Drawing.Color.Crimson;
+            this.nSubtract.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.nSubtract.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.nSubtract.Font = new System.Drawing.Font("Arial", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nSubtract.Location = new System.Drawing.Point(229, 170);
+            this.nSubtract.Name = "nSubtract";
+            this.nSubtract.Size = new System.Drawing.Size(64, 62);
+            this.nSubtract.TabIndex = 12;
+            this.nSubtract.Text = "-";
+            this.nSubtract.UseVisualStyleBackColor = false;
+            this.nSubtract.Click += new System.EventHandler(this.nSubtract_Click);
+            // 
+            // nMultiply
+            // 
+            this.nMultiply.BackColor = System.Drawing.Color.Crimson;
+            this.nMultiply.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.nMultiply.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.nMultiply.Font = new System.Drawing.Font("Goudy Old Style", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nMultiply.Location = new System.Drawing.Point(229, 238);
+            this.nMultiply.Name = "nMultiply";
+            this.nMultiply.Size = new System.Drawing.Size(64, 62);
+            this.nMultiply.TabIndex = 13;
+            this.nMultiply.Text = "*";
+            this.nMultiply.UseVisualStyleBackColor = false;
+            this.nMultiply.Click += new System.EventHandler(this.nMultiply_Click);
+            // 
+            // nPoint
+            // 
+            this.nPoint.BackColor = System.Drawing.Color.Crimson;
+            this.nPoint.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.nPoint.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.nPoint.Font = new System.Drawing.Font("Goudy Old Style", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nPoint.Location = new System.Drawing.Point(13, 306);
+            this.nPoint.Name = "nPoint";
+            this.nPoint.Size = new System.Drawing.Size(64, 62);
+            this.nPoint.TabIndex = 14;
+            this.nPoint.Text = ".";
+            this.nPoint.UseVisualStyleBackColor = false;
+            this.nPoint.Click += new System.EventHandler(this.nPoint_Click);
+            // 
+            // n0
+            // 
+            this.n0.BackColor = System.Drawing.Color.Crimson;
+            this.n0.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.n0.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.n0.Font = new System.Drawing.Font("Goudy Old Style", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n0.Location = new System.Drawing.Point(83, 306);
+            this.n0.Name = "n0";
+            this.n0.Size = new System.Drawing.Size(64, 62);
+            this.n0.TabIndex = 15;
+            this.n0.Text = "0";
+            this.n0.UseVisualStyleBackColor = false;
+            this.n0.Click += new System.EventHandler(this.n0_Click);
+            // 
+            // nCE
+            // 
+            this.nCE.BackColor = System.Drawing.Color.Crimson;
+            this.nCE.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.nCE.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.nCE.Font = new System.Drawing.Font("Goudy Old Style", 21F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nCE.Location = new System.Drawing.Point(153, 306);
+            this.nCE.Name = "nCE";
+            this.nCE.Size = new System.Drawing.Size(64, 62);
+            this.nCE.TabIndex = 16;
+            this.nCE.Text = "CE";
+            this.nCE.UseVisualStyleBackColor = false;
+            // 
+            // nDivide
+            // 
+            this.nDivide.BackColor = System.Drawing.Color.Crimson;
+            this.nDivide.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.nDivide.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.nDivide.Font = new System.Drawing.Font("Goudy Old Style", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nDivide.Location = new System.Drawing.Point(229, 306);
+            this.nDivide.Name = "nDivide";
+            this.nDivide.Size = new System.Drawing.Size(64, 62);
+            this.nDivide.TabIndex = 17;
+            this.nDivide.Text = "/";
+            this.nDivide.UseVisualStyleBackColor = false;
+            this.nDivide.Click += new System.EventHandler(this.nDivide_Click);
+            // 
+            // nClear
+            // 
+            this.nClear.BackColor = System.Drawing.Color.Pink;
+            this.nClear.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.nClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.nClear.Font = new System.Drawing.Font("Goudy Old Style", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nClear.Location = new System.Drawing.Point(229, 379);
+            this.nClear.Name = "nClear";
+            this.nClear.Size = new System.Drawing.Size(63, 48);
+            this.nClear.TabIndex = 18;
+            this.nClear.Text = "ClrScr";
+            this.nClear.UseVisualStyleBackColor = false;
+            this.nClear.Click += new System.EventHandler(this.nClear_Click);
+            // 
+            // n3
+            // 
+            this.n3.BackColor = System.Drawing.Color.Crimson;
+            this.n3.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.n3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.n3.Font = new System.Drawing.Font("Goudy Old Style", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n3.Location = new System.Drawing.Point(153, 102);
+            this.n3.Name = "n3";
+            this.n3.Size = new System.Drawing.Size(64, 62);
+            this.n3.TabIndex = 19;
+            this.n3.Text = "3";
+            this.n3.UseVisualStyleBackColor = false;
+            this.n3.Click += new System.EventHandler(this.n3_Click);
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.IndianRed;
+            this.ClientSize = new System.Drawing.Size(304, 439);
+            this.Controls.Add(this.n3);
+            this.Controls.Add(this.nClear);
+            this.Controls.Add(this.nDivide);
+            this.Controls.Add(this.nCE);
+            this.Controls.Add(this.n0);
+            this.Controls.Add(this.nPoint);
+            this.Controls.Add(this.nMultiply);
+            this.Controls.Add(this.nSubtract);
+            this.Controls.Add(this.nAdd);
+            this.Controls.Add(this.nEquals);
+            this.Controls.Add(this.n9);
+            this.Controls.Add(this.n8);
+            this.Controls.Add(this.n7);
+            this.Controls.Add(this.n6);
+            this.Controls.Add(this.n5);
+            this.Controls.Add(this.n4);
+            this.Controls.Add(this.n2);
+            this.Controls.Add(this.n1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Calculator";
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.Calculator_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button n1;
+        private System.Windows.Forms.Button n2;
+        private System.Windows.Forms.Button n4;
+        private System.Windows.Forms.Button n5;
+        private System.Windows.Forms.Button n6;
+        private System.Windows.Forms.Button n7;
+        private System.Windows.Forms.Button n8;
+        private System.Windows.Forms.Button n9;
+        private System.Windows.Forms.Button nEquals;
+        private System.Windows.Forms.Button nAdd;
+        private System.Windows.Forms.Button nSubtract;
+        private System.Windows.Forms.Button nMultiply;
+        private System.Windows.Forms.Button nPoint;
+        private System.Windows.Forms.Button n0;
+        private System.Windows.Forms.Button nCE;
+        private System.Windows.Forms.Button nDivide;
+        private System.Windows.Forms.Button nClear;
+        private System.Windows.Forms.Button n3;
+    }
+}
+
---
+++ MyApp/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyApp
+{
+    public partial class Calculator : Form
+    {
+        int i = 0;
+        double fNumber=0, sNumber=0, res=0;
+        string op="";
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "9";
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "7";
+
+        }
+
+        private void Calculator_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void n1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "1";
+
+        }
+
+        private void n2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "2";
+
+        }
+
+        private void n3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "3";
+
+        }
+
+        private void n4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "4";
+
+        }
+
+        private void n5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "5";
+
+        }
+
+        private void n6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "6";
+
+        }
+
+        private void n8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "8";
+
+        }
+
+        private void n0_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "0";
+
+        }
+
+        private void nPoint_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + ".";
+
+        }
+
+        private void nEquals_Click(object sender, EventArgs e)
+        {
+            sNumber = Convert.ToDouble(textBox1.Text);
+            i = 1;
+            switch(op)
+            {
+                case "+":
+                    res = fNumber + sNumber;
+                    textBox1.Text = Convert.ToString(res);
+                    break;
+                case "-":
+                    res = fNumber - sNumber;
+                    textBox1.Text = Convert.ToString(res);
+                    break;
+                case "*":
+                    res = fNumber * sNumber;
+                    textBox1.Text = Convert.ToString(res);
+                    break;
+                case "/":
+                    if (sNumber==0)
+                    {
+                        textBox1.Text = "Math Error!";
+                    }
+                    else
+                    {
+                        res = fNumber / sNumber;
+                        textBox1.Text = Convert.ToString(res);
+                    }
+                    break;
+            }
+        }
+
+        private void nSubtract_Click(object sender, EventArgs e)
+        {
+            op = "-";
+            fNumber = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "";
+        }
+
+        private void nMultiply_Click(object sender, EventArgs e)
+        {
+            op = "";
+            fNumber = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "*";
+        }
+
+        private void nDivide_Click(object sender, EventArgs e)
+        {
+            op = "";
+            fNumber = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "/";
+        }
+
+        private void nClear_Click(object sender, EventArgs e)
+        {
+            //textBox1.Text = "0";
+            textBox1.Text = "";
+
+        }
+
+        private void nAdd_Click(object sender, EventArgs e)
+        {
+            op = "+";
+            fNumber = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "";
+
+        }
+    }
+}
